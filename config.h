eb7ef3484972ca784ed44ff3ff01536fa61a3e8c
Noel Cower <ncower@gmail.com>
2011-10-01 18:11:15 -0600

    Basic configuration header

---
+++ config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PLATFORM_UNIX (defined(unix) || defined(__unix) || defined(__unix__))
+#define PLATFORM_APPLE (defined(__APPLE__))
+#define PLATFORM_WINDOWS (defined(_WIN32) || defined(__MINGW32__))
+#define PLATFORM_LINUX (defined(__linux__) || defined(linux) || defined(__linux))
+
+#if !defined(USE_PTHREADS)
+#define USE_PTHREADS (PLATFORM_UNIX || PLATFORM_APPLE || PLATFORM_LINUX)
+#endif /* !defined(USE_PTHREADS) */
+
+#if USE_PTHREADS
+#include <pthread.h>
+#endif /* USE_PTHREADS */
+
+#include "logging.h"
+
+#endif /* end of include guard: CONFIG_H */
