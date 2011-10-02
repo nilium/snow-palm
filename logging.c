1d093266ae00402a60b6c1e34b4fb6363ba65fb2
Noel Cower <ncower@gmail.com>
2011-10-01 18:10:52 -0600

    Added logging routines from snow palm

---
+++ logging.c
@@ -0,0 +1,24 @@
+#include "logging.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+void log_fatal_(const char *format, int error, ...)
+{
+	va_list args;
+	va_start(args, error);
+	vfprintf(stderr, format, args);
+	va_end(args);
+	fprintf(stderr, "Exiting with error code %d\n", error);
+	fflush(stderr);
+	exit(error);
+} /* log_fatal */
+
+#if defined(__cplusplus)
+}
+#endif
+
---
+++ logging.h
@@ -0,0 +1,84 @@
+#ifndef LOG_H_B31TON6M
+#define LOG_H_B31TON6M
+
+#include <stdio.h>
+
+/*! Force the inclusion of logging macros regardless of release/debug mode. */
+#if !defined(FORCE_LOGGING)
+#define FORCE_LOGGING 0
+#endif
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+/*! Writes an error message to stderr then kills the program with the given
+ *  error code.  This should be used only when an error absolutely cannot be
+ *  recovered from.  It should be obvious, but this function does not return.
+ *  Preferably, you should use the ::log_fatal macro to additionally pass file
+ *  and line number information along with the error message.
+ *
+ *  @param[in] format The format for the error message.
+ *  @param[in] error The error code to exit with.
+ */
+void log_fatal_(const char *format, int error, ...);
+
+/*! \brief Macro around ::log_fatal_ to pass in additional file, line number,
+	and callee information to the format string.  This is never a no-op.
+*/
+#define log_fatal(FORMAT, ERROR, args...) log_fatal_("Fatal Error [%s:%s:%d]: " FORMAT, (ERROR), __FILE__, __FUNCTION__, __LINE__, ##args)
+
+/* if debugging or logging is forcibly enabled, then provide additional logging macros */
+#if !defined(NDEBUG) || FORCE_LOGGING
+#if !defined(log_error)
+#define log_error(FORMAT, args...)   fprintf(stderr, "Error [%s:%s:%d]: "   FORMAT, __FILE__, __FUNCTION__, __LINE__, ##args)
+#endif
+#if !defined(log_warning)
+#define log_warning(FORMAT, args...) fprintf(stderr, "Warning [%s:%s:%d]: " FORMAT, __FILE__, __FUNCTION__, __LINE__, ##args)
+#endif
+#if !defined(log_note)
+#define log_note(FORMAT, args...)    fprintf(stderr, "Note [%s:%s:%d]: "    FORMAT, __FILE__, __FUNCTION__, __LINE__, ##args)
+#endif
+#else
+/* unless previously defined, log macros do nothing in release builds */
+#if !defined(log_error)
+#define log_error(FORMAT, args...) 
+#endif
+#if !defined(log_warning)
+#define log_warning(FORMAT, args...) 
+#endif
+#if !defined(log_note)
+#define log_note(FORMAT, args...) 
+#endif
+#endif
+
+/*! \def log_error(FORMAT, args...)
+	\brief Macro to write an error message with the given format and parameters
+	to stderr.  Includes file, line number, and callee information in the format
+	string automatically.
+	
+	In builds with NDEBUG defined, this will be a no-op.
+*/
+
+/*! \def log_warning(FORMAT, args...)
+	\brief Macro to write an warning message with the given format and parameters
+	to stderr.  Includes file, line number, and callee information in the format
+	string automatically.
+	
+	In builds with NDEBUG defined, this will be a no-op.
+*/
+
+/*! \def log_note(FORMAT, args...)
+	\brief Macro to write a general message with the given format and parameters
+	to stderr.  Includes file, line number, and callee information in the format
+	string automatically.
+	
+	In builds with NDEBUG defined, this will be a no-op.
+*/
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* end of include guard: LOG_H_B31TON6M */
