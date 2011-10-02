c16a7dd1d1942537092e86d37d67451a7d418ffe
Noel Cower <ncower@gmail.com>
2011-10-01 18:36:47 -0600

    Adding initial threading routines.
    Will end up being similar to mutex_t re: error checking.

---
+++ thread.h
@@ -0,0 +1,71 @@
+#ifndef THREAD_H
+
+#define THREAD_H
+
+#include "config.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif /* __cplusplus */
+
+typedef void *(*thread_fn_t)(void *context);
+
+#if USE_PTHREADS
+typedef pthread_t thread_t;
+
+/*! Initializes a thread. */
+inline void thread_create(thread_t *restrict thread, thread_fn_t fn, void *restrict context)
+{
+	pthread_create(thread, NULL, fn, context);
+}
+
+inline void thread_kill(thread_t thread)
+{
+	pthread_cancel(thread);
+}
+
+inline int thread_equals(thread_t left, thread_t right)
+{
+	return pthread_equal(left, right);
+}
+
+inline void thread_detach(thread_t thread)
+{
+	pthread_detach(thread);
+}
+
+inline void thread_join(thread_t thread, void **returnValue)
+{
+	pthread_join(thread, returnValue);
+}
+
+inline void thread_exit(void *returnValue)
+{
+	pthread_exit(returnValue);
+}
+
+inline thread_t thread_current_thread(void)
+{
+	return pthread_self();
+}
+
+#else /* USE_PTHREADS */
+
+/*! Initializes a thread. */
+void thread_create(thread_t *thread, thread_fn_t fn, void *context);
+void thread_create(thread_t *thread, thread_fn_t fn, void *context);
+void thread_kill(thread_t thread);
+int thread_equals(thread_t left, thread_t right);
+void thread_detach(thread_t thread);
+void thread_join(thread_t thread, void **returnValue);
+void thread_exit(void *returnValue);
+thread_t thread_current_thread(void);
+
+#endif
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* end of include guard: THREAD_H */
