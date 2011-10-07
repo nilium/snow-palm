d86af1fe223451c4768a86f3c175a4bb86cf6cdb
Noel Cower <ncower@gmail.com>
2011-10-07 01:40:13 -0600

    Added opaque[] flexible array member to s_class - this requires C99, but allows any number of additional functions to be added to the VMT. Added retain map/lock as well as retain/release/retainCount implementations.  autorelease does nothing right now since AR pools aren't even implemented.  Debating whether I should just remove autoreleasing or not. Implemented basic object_compare (difference of addresses).

--- src/object.c
+++ src/object.c
@@ -1,5 +1,6 @@
 #include "object.h"
 #include "map.h"
+#include "mutex.h"
 
 #if defined(__cplusplus)
 extern "C"
@@ -21,6 +22,22 @@ const class_t object_class = {
 	object_retainCount
 };
 
+static map_t g_retain_map; /* kv-pair: (object address, (void *)-sized integer) */
+static mutex_t g_retain_lock;
+
+void sys_object_init(void)
+{
+	mutex_init(&g_retain_lock, 1);
+	map_init(&g_retain_map, NULL);
+	atexit(sys_object_shutdown);
+}
+
+void sys_object_shutdown(void)
+{
+	map_destroy(&g_retain_map);
+	mutex_destroy(&g_retain_lock);
+}
+
 object_t object_ctor(object_t self)
 {
 	/* retain count 1, NOP */
@@ -29,15 +46,35 @@ object_t object_ctor(object_t self)
 
 void object_dtor(object_t self)
 {
+	/* TODO: weak reference table, signal object deletion */
 }
 
 int object_compare(object_t self, object_t other)
 {
-	return -1;
+	ptrdiff_t diff = self - other;
+	if (diff < 0)
+		return -1;
+	else if (diff > 0)
+		return 1;
+	else
+		return 0;
 }
 
 object_t object_retain(object_t self)
 {
+	uint32_t *retainCount;
+	int found;
+
+	mutex_lock(&g_retain_lock);
+
+	map_getAddr(&g_retain_map, self, &retainCount);
+	if (found) {
+		*retainCount += 1;
+	} else {
+		map_insert(&g_retain_map, self, (void *)((uint32_t)1));
+	}
+
+	mutex_unlock(&g_retain_lock);
 	return self;
 }
 
@@ -48,11 +85,42 @@ object_t object_autorelease(object_t self)
 
 void object_release(object_t self)
 {
+	uint32_t *retainCount;
+	int found;
+
+	mutex_lock(&g_retain_lock);
+
+	map_getAddr(&g_retain_map, self, &retainCount);
+	if (found) {
+		int count = (*retainCount) - 1;
+		
+		if (count == 1)
+			map_remove(&g_retain_map, self);
+		else
+			*retainCount = count;
+	}
+
+	mutex_unlock(&g_retain_lock);
+
+	/* if no entry in the map is found, the object's retain count
+	   was 1, and is now 0, so delete it
+	*/
+	if (!found) object_delete(self);
 }
 
 int32_t object_retainCount(object_t self)
 {
-	return 1;
+	uint32_t retainCount;
+	int found;
+
+	mutex_lock(&g_retain_lock);
+
+	map_get(&g_retain_map, self, (void **)&retainCount);
+	if (!found) retainCount = 1;
+
+	mutex_unlock(&g_retain_lock);
+
+	return retainCount;
 }
 
 object_t object_new(class_t *cls, memory_pool_t *pool)
