976e5fb7746ff274f3f0ca085b03fbc2c806890b
Noel Cower <ncower@gmail.com>
2011-10-07 01:44:37 -0600

    commented out entity bits of main.c for now, updated to use sys_ prefix on memory pool init.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,17 @@
 #include "config.h"
 #include "memory.h"
-#include "entity.h"
+//#include "entity.h"
 
 static void main_shutdown(void)
 {
-	sys_entity_shutdown();
-	mem_shutdown();
+//	sys_entity_shutdown();
+	sys_mem_shutdown();
 }
 
 int main(int argc, const char *argv[])
 {
-	mem_init();
-	sys_entity_init();
+	sys_mem_init();
+//	sys_entity_init();
 	atexit(main_shutdown);
 	
 	/* do stuff! */
