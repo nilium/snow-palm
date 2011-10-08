1c6910243a0a378db3ce8e1a9588f87300417f5d
Noel Cower <ncower@gmail.com>
2011-10-08 00:46:53 -0600

    Adding testing code to main.c.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,28 @@
 #include "config.h"
 #include "memory.h"
-//#include "entity.h"
+#include "entity.h"
 
 static void main_shutdown(void)
 {
-//	sys_entity_shutdown();
+	sys_entity_shutdown();
+    sys_object_shutdown();
 	sys_mem_shutdown();
 }
 
 int main(int argc, const char *argv[])
 {
 	sys_mem_init();
-//	sys_entity_init();
+    sys_object_init();
+	sys_entity_init();
 	atexit(main_shutdown);
+
+
+	entity_t *entity = entity_new("foo", NULL);
+	entity_t *child = entity_new("bar", entity);
+	entity_t *child2 = entity_new("baz", entity);
+	object_release(entity);
+	object_release(child);
+	object_release(child2);
 	
 	/* do stuff! */
 
