30e5eee5bef08b2d0743a0a41926977782fdf528
Noel Cower <ncower@gmail.com>
2011-10-08 00:36:17 -0600

    Bunch of updates to the entity class.  Rotations are now entirely in quaternions, more transform mutators/accessors have been added and implemented, the transform in general should be function now, and parenting should work.  None of this is actually useful unless rendering is implemented though.

--- src/entity.h
+++ src/entity.h
@@ -15,17 +15,14 @@ extern "C"
 typedef enum
 {
 	DIRTY_TRANSFORM		=0x1<<0,	/*! Transformation matrix is dirty. */
-	DIRTY_POSITION		=0x1<<1,	/*! Position is dirty. */
-	DIRTY_ROTATION		=0x1<<2, 	/*! Rotation is dirty. */
-	DIRTY_SCALE			=0x1<<3, 	/*! Scale is dirty. */
-	DIRTY_WORLD			=0x1<<4, 	/*! World transform is dirty. */
+	DIRTY_WORLD			=0x1<<1, 	/*! World transform is dirty. */
 	/*! Combination of all DIRTY_ flags. */
-	DIRTY_FLAGS			=(DIRTY_TRANSFORM|DIRTY_POSITION|DIRTY_ROTATION|DIRTY_SCALE|DIRTY_WORLD),
+	DIRTY_FLAGS			=(DIRTY_TRANSFORM|DIRTY_WORLD),
 
-	ROOT_ENTITY			=0x1<<5,	/*! Entity is a root entity. */
+	ROOT_ENTITY			=0x1<<2,	/*! Entity is a root entity. */
 	
-	ENTITY_DISABLED		=0x1<<6,	/*! Entity is currently disabled. */
-	ENTITY_HIDDEN		=0x1<<7,	/*! Entity is currently hidden. */
+	ENTITY_DISABLED		=0x1<<3,	/*! Entity is currently disabled. */
+	ENTITY_HIDDEN		=0x1<<4,	/*! Entity is currently hidden. */
 } entity_flag_t;
 
 typedef struct s_entity entity_t;
@@ -34,16 +31,22 @@ struct s_entity
 {
 	class_t *isa;
 
-	list_t children;
 	list_t components;
+
+	list_t children;
+	listnode_t *parentnode;
 	entity_t *parent;
+
 	char *name;
+	
 	/*! internal flags */
 	entity_flag_t _iflags;
+
+	mat4_t worldTransform;
 	mat4_t transform;
-	mat4_t _world_transform;
 	quat_t rotation;
-	vec3_t position, scale;
+	vec3_t position;
+	vec3_t scale;
 };
 
 extern const class_t entity_class;
@@ -51,7 +54,7 @@ extern const class_t entity_class;
 void sys_entity_init(void);
 void sys_entity_shutdown(void);
 
-entity_t *entity_new(const char *name);
+entity_t *entity_new(const char *name, entity_t *parent);
 
 void entity_addChild(entity_t *self, entity_t *child);
 void entity_removeFromParent(entity_t *self);
@@ -59,12 +62,33 @@ void entity_removeFromParent(entity_t *self);
 void entity_setName(entity_t *self, const char *name);
 const char *entity_getName(const entity_t *self);
 
+/*! \brief Sets the position of the entity relative to its parent. */
+void entity_position(entity_t *self, float x, float y, float z);
+/*! \brief Moves the entity relative to its orientation and parent. */
 void entity_move(entity_t *self, float x, float y, float z);
+/*! \brief Translates the entity relative to its parent (ignores orientation). */
 void entity_translate(entity_t *self, float x, float y, float z);
-void entity_rotate(entity_t *self, float x, float y, float z);
-void entity_turn(entity_t *self, float x, float y, float z);
+
+/*! \brief Sets the rotation of the entity (relative to its parent). */
+void entity_rotate(entity_t *self, quat_t rot);
+/*! \brief Rotates the entity by pitch, yaw, and roll (relative to its parent) - this concatenates rotations. */
+void entity_turn(entity_t *self, quat_t rot);
+
+/*! \brief Sets the scale of the entity.  Has no direct effect on entity behavior,
+    but may modify component behavior.
+*/
 void entity_scale(entity_t *self, float x, float y, float z);
+
+/*! Gets the entity's transformation matrix. */
+void entity_getTransform(entity_t *self, mat4_t out);
+/*! Gets the entity's world transformation matrix. */
+void entity_getWorldTransform(entity_t *self, mat4_t out);
+/*! Gets the entity's scale (relative to its parent). */
 void entity_getScale(entity_t *self, float *x, float *y, float *z);
+/*! Gets the entity's rotation (relative to its parent). */
+void entity_getRotation(entity_t *self, quat_t out);
+/*! Gets the entity's position (relative to its parent). */
+void entity_getPosition(entity_t *self, float *x, float *y, float *z);
 
 #if defined(__cplusplus)
 }
