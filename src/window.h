46c7b473b27fc0c78af1b1c7f6fc9a5dc3805d43
Noel Cower <ncower@gmail.com>
2011-10-08 20:32:41 -0600

    Adding barebones window-creation routines.
    These technically work on other platforms provided you link with the
    libraries provided in the PDK - outside of the PDK, they won't work.
    Not really important to target anything other than the PDK right now,
    though.  Note: this breaks host compilation right now.

---
+++ src/window.h
@@ -0,0 +1,19 @@
+#ifndef WINDOW_H
+
+#define WINDOW_H
+
+#include "config.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif /* __cplusplus */
+
+bool window_create(void);
+void window_destroy(void);
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* end of include guard: WINDOW_H */
