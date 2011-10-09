6f8a96e0e15ceaaba568dd3d7e913fcff3754ea5
Noel Cower <ncower@gmail.com>
2011-10-08 20:32:41 -0600

    Barebones window-creation routines.  These technically work on other platforms provided you link with the libraries provided in the PDK - outside of the PDK, they won't work.  Not really important to target anything other than the PDK right now, though.  Note: this breaks host compilation right now.

---
+++ src/window_touchpad.c
@@ -0,0 +1,39 @@
+#include "window.h"
+#include <SDL/SDL.h>
+
+#if PLATFORM_TOUCHPAD
+#include <PDL.h>
+#endif
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif /* __cplusplus */
+
+static struct {
+	SDL_Surface *surface;
+} g_wnd;
+
+bool window_create(void)
+{
+	/* init SDL and PDL */
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE);
+	PDL_Init(0);
+	
+	/* prep for OpenGL ES 2 */
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	
+	g_wnd.surface = SDL_SetVideoMode(0, 0, 0, SDL_OPENGL);
+
+	return true;
+}
+
+void window_destroy(void)
+{
+	PDL_Quit();
+	SDL_Quit();
+}
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
