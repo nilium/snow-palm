ac7e071fbaa5e3dc98a6f3b62241a92b7455b931
Noel Cower <ncower@gmail.com>
2011-10-08 20:00:10 -0600

    Switching logging to use PDL_Log on the Touchpad.

--- src/logging.c
+++ src/logging.c
@@ -18,10 +18,22 @@ void log_fatal_(const char *format, int error, ...)
 {
 	va_list args;
 	va_start(args, error);
+#if PLATFORM_TOUCHPAD
+	int length = vsprintf(format, args) + 1;
+	char *str = (char *)malloc(size(char) * length);
+	va_end(args);
+	va_start(args, error);
+	vsnprintf(str, length, format, args);
+	va_end(args);
+	PDL_Log("%s", str);
+	free(str);
+	PDL_Log("Exiting with error code %d\n", error);
+#else
 	vfprintf(stderr, format, args);
 	va_end(args);
 	fprintf(stderr, "Exiting with error code %d\n", error);
 	fflush(stderr);
+#endif
 	exit(error);
 } /* log_fatal */
 
