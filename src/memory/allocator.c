9e962b18bd53d2ef5a50bcb60d03f984c04acb80
Noel Cower <ncower@gmail.com>
2012-06-03 01:14:35 -0700

    Adding com_* memory functions.
    com_ memory routines are just wrappers that take an allocator_t and
    the function's respective arguments and passes the allocator's context
    along to the allocator's functions.  Essentially, they are convenience
    functions that hopefully simplify things.  As is, the whole allocator
    scheme is tedious with contexts, but sufficient for any purpose I can
    imagine right now.  On the upside, if allocator_t changes down the road
    then the com_* calls will be perfectly fine as I update them while the
    calls that directly access an allocator_t may not be.
    
    May remove realloc down the road.  It seems like it won't get much use.
    
    Additionally, added the context argument to the functions.  Default
    implementation still ignores them, though includes wrapper functions to
    keep things correct.

--- src/memory/allocator.c
+++ src/memory/allocator.c
@@ -1,9 +1,24 @@
 #include "allocator.h"
 #include <stdlib.h>
 
+static void *sn_malloc(size_t min_size, void *ctx) {
+  (void)ctx;
+  return malloc(min_size);
+}
+
+static void sn_free(void *p, void *ctx) {
+  (void)ctx;
+  free(p);
+}
+
+static void *sn_realloc(void *p, size_t min_size, void *ctx) {
+  (void)ctx;
+  return realloc(p, min_size);
+}
+
 allocator_t default_allocator() {
   static allocator_t g_default_allocator = {
-    free, malloc, realloc, NULL
+    sn_malloc, sn_realloc, sn_free, NULL
   };
   return g_default_allocator;
 }
--- src/memory/allocator.h
+++ src/memory/allocator.h
@@ -1,16 +1,28 @@
 #ifndef __SNOW_ALLOCATOR_H__
 #define __SNOW_ALLOCATOR_H__ 1
 
-typedef void (*free_fn_t)(void *);
-typedef void *(*malloc_fn_t)(size_t);
-typedef void *(*realloc_fn_t)(void *, size_t);
+typedef void (*free_fn_t)(void *p, void *context);
+typedef void *(*malloc_fn_t)(size_t min_size, void *context);
+typedef void *(*realloc_fn_t)(void *p, size_t min_size, void *context);
 
 typedef struct s_allocator {
-  free_fn_t free;
   malloc_fn_t malloc;
   realloc_fn_t realloc;
+  free_fn_t free;
   void *context;
 } allocator_t;
+
+inline void *com_malloc(allocator_t alloc, size_t min_size) {
+  return alloc.malloc(min_size, alloc.context);
+}
+
+inline void *com_realloc(allocator_t alloc, void *p, size_t min_size) {
+  return alloc.realloc(p, min_size, alloc.context);
+}
+
+inline void com_free(allocator_t alloc, void *p) {
+  return alloc.free(p, alloc.context);
+}
 
 allocator_t default_allocator(void);
 
