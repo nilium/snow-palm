d5db9e53422b7e2bcc67c2b2f9ee177937e585eb
Noel Cower <ncower@gmail.com>
2012-06-02 17:13:08 -0700

    Updating maths files.
    * Replacing use of base types with s_float_t (mostly so it can be swapped
    out on systems where using either float/double would be advantageous).
    * Renaming matrix to mat4.
    * Removing other maths/ includes from maths.h.
    * Renamed expand macros to splat.

--- src/maths/quat.h
+++ src/maths/quat.h
@@ -20,7 +20,7 @@ extern const quat_t g_quat_identity;
 
 /* note: all methods assume that input quaternions are unit quaternions */
 
-void quat_set(float x, float y, float z, float w, quat_t out);
+void quat_set(s_float_t x, s_float_t y, s_float_t z, s_float_t w, quat_t out);
 void quat_copy(const quat_t in, quat_t out);
 void quat_identity(quat_t q);
 
@@ -30,10 +30,10 @@ void quat_negate(const quat_t in, quat_t out);
 void quat_multiply(const quat_t left, const quat_t right, quat_t out);
 void quat_multiply_vec3(const quat_t left, const vec3_t right, vec3_t out);
 
-void quat_from_angle_axis(float angle, float x, float y, float z, quat_t out);
+void quat_from_angle_axis(s_float_t angle, s_float_t x, s_float_t y, s_float_t z, quat_t out);
 void quat_from_mat4(const mat4_t mat, quat_t out);
 
-void quat_slerp(const quat_t from, const quat_t to, float delta, quat_t out);
+void quat_slerp(const quat_t from, const quat_t to, s_float_t delta, quat_t out);
 
 int quat_equals(const quat_t left, const quat_t right);
 
--- src/maths/vec4.c
+++ src/maths/vec4.c
@@ -17,96 +17,107 @@ const vec4_t g_vec4_one = {1.0, 1.0, 1.0, 1.0};
 
 void vec4_copy(const vec4_t in, vec4_t out)
 {
-	out[0] = in[0];
-	out[1] = in[1];
-	out[2] = in[2];
-	out[3] = in[3];
+  out[0] = in[0];
+  out[1] = in[1];
+  out[2] = in[2];
+  out[3] = in[3];
 }
 
-void vec4_set(float x, float y, float z, float w, vec4_t v)
+void vec4_set(s_float_t x, s_float_t y, s_float_t z, s_float_t w, vec4_t v)
 {
-	v[0] = x;
-	v[1] = y;
-	v[2] = z;
-	v[3] = w;
+  v[0] = x;
+  v[1] = y;
+  v[2] = z;
+  v[3] = w;
 }
 
-float vec4_length_squared(const vec4_t v)
+/*!
+ * Gets the squared length of a vector.  Useful for approximations and when
+ * you don't need the actual magnitude.
+ */
+s_float_t vec4_length_squared(const vec4_t v)
 {
-	return (v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]) + (v[3] * v[3]);
+  return (v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]) + (v[3] * v[3]);
 }
 
-float vec4_length(const vec4_t v)
+/*!
+ * Gets the length/magnitude of a vector.
+ */
+s_float_t vec4_length(const vec4_t v)
 {
-	return sqrtf((v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]) + (v[3] * v[3]));
+  return sqrtf((v[0] * v[0]) + (v[1] * v[1]) + (v[2] * v[2]) + (v[3] * v[3]));
 }
 
 void vec4_normalize(const vec4_t in, vec4_t out)
 {
-	float mag = vec4_length(in);
-	if (mag) mag = 1.0 / mag;
-	out[0] = in[0] * mag;
-	out[1] = in[1] * mag;
-	out[2] = in[2] * mag;
-	out[3] = in[3] * mag;
+  s_float_t mag = vec4_length(in);
+  if (mag) mag = 1.0 / mag;
+  out[0] = in[0] * mag;
+  out[1] = in[1] * mag;
+  out[2] = in[2] * mag;
+  out[3] = in[3] * mag;
 }
 
 void vec4_subtract(const vec4_t left, const vec4_t right, vec4_t out)
 {
-	out[0] = left[0] - right[0];
-	out[1] = left[1] - right[1];
-	out[2] = left[2] - right[2];
-	out[3] = left[3] - right[3];
+  out[0] = left[0] - right[0];
+  out[1] = left[1] - right[1];
+  out[2] = left[2] - right[2];
+  out[3] = left[3] - right[3];
 }
 
 void vec4_add(const vec4_t left, const vec4_t right, vec4_t out)
 {
-	out[0] = left[0] + right[0];
-	out[1] = left[1] + right[1];
-	out[2] = left[2] + right[2];
-	out[3] = left[3] + right[3];
+  out[0] = left[0] + right[0];
+  out[1] = left[1] + right[1];
+  out[2] = left[2] + right[2];
+  out[3] = left[3] + right[3];
 }
 
 void vec4_multiply(const vec4_t left, const vec4_t right, vec4_t out)
 {
-	out[0] = left[0] * right[0];
-	out[1] = left[1] * right[1];
-	out[2] = left[2] * right[2];
-	out[3] = left[3] * right[3];
+  out[0] = left[0] * right[0];
+  out[1] = left[1] * right[1];
+  out[2] = left[2] * right[2];
+  out[3] = left[3] * right[3];
 }
 
 void vec4_invert(vec4_t v)
 {
-	v[0] = -v[0];
-	v[1] = -v[1];
-	v[2] = -v[2];
-	v[3] = -v[3];
+  v[0] = -v[0];
+  v[1] = -v[1];
+  v[2] = -v[2];
+  v[3] = -v[3];
 }
 
-float vec4_dot_product(const vec4_t left, const vec4_t right)
+s_float_t vec4_dot_product(const vec4_t left, const vec4_t right)
 {
-	return ((left[0] * right[0]) + (left[1] * right[1]) + (left[2] * right[2]) + (left[3] * right[3]));
+  return ((left[0] * right[0]) + (left[1] * right[1]) + (left[2] * right[2]) + (left[3] * right[3]));
 }
 
-void vec4_scale(float scalar, vec4_t v)
+void vec4_scale(s_float_t scalar, vec4_t v)
 {
-	v[0] *= scalar;
-	v[1] *= scalar;
-	v[2] *= scalar;
-	v[3] *= scalar;
+  v[0] *= scalar;
+  v[1] *= scalar;
+  v[2] *= scalar;
+  v[3] *= scalar;
 }
 
-int vec4_divide(float divisor, vec4_t v)
+/*!
+ * Divides the given vector by the divisor.
+ * \returns Zero if the result is undefined, otherwise non-zero.
+ */
+int vec4_divide(s_float_t divisor, vec4_t v)
 {
-	if (divisor) {
-		divisor = 1.0 / divisor;
-		v[0] *= divisor;
-		v[1] *= divisor;
-		v[2] *= divisor;
-		v[3] *= divisor;
-		return 1;
-	}
-	return 0;
+  if (divisor) {
+    divisor = 1.0 / divisor;
+    v[0] *= divisor;
+    v[1] *= divisor;
+    v[2] *= divisor;
+    v[3] *= divisor;
+    return 1;
+  }
+  return 0;
 }
 
 #if defined(__cplusplus)
