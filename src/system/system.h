60da843db350954606da7bec8af4ea762e578c06
Noel Cower <ncower@gmail.com>
2012-06-02 17:35:43 -0700

    Adding system.h
    This is part of the tear-up process.  Basically, this defines the new
    entrypoint for the program, the init routine, frame routine, etc.

---
+++ src/system/system.h
@@ -0,0 +1,20 @@
+#ifndef __SNOW_SYSTEM_H__
+#define __SNOW_SYSTEM_H__ 1
+
+void sys_main(int argc, const char *argv[]);
+
+// routine that initializes the game/app
+// returns 0 if successful
+int sys_init();
+// routine that runs a frame in the game/app
+// returns 0 if successful
+int sys_frame(const s_time_t frame_time);
+// routine that prepares the app/game to shut down
+// has no success/failure conditions - if the engine crashes at this point, it
+// was going down anyway, just make sure to log it
+void sys_quit();
+
+// tells the app/game to shut down right now
+void sys_terminate();
+
+#endif /* end __SNOW_SYSTEM_H__ include guard */
