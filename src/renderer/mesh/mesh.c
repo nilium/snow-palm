5e8834c822d5977d6376bc9fe99ea5c5d17af4e9
Noel Cower <ncower@gmail.com>
2012-06-14 20:46:46 -0700

    Add base contents of mesh.*

---
+++ src/renderer/mesh/mesh.c
@@ -0,0 +1,11 @@
+#include "mesh.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
---
+++ src/renderer/mesh/mesh.h
@@ -0,0 +1,16 @@
+#ifndef __SNOW_MESH_H__
+#define __SNOW_MESH_H__ 1
+
+#include <snow-config.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif /* end __SNOW_MESH_H__ include guard */
