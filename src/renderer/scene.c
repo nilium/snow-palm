94fadde1c404227e9c7581f983920f3fae6a7f91
Noel Cower <ncower@gmail.com>
2012-06-20 14:43:55 -0700

    scene: Initial commit.

---
+++ src/renderer/scene.c
@@ -0,0 +1,15 @@
+#define __SNOW_SCENE_C__ 1
+
+#include "scene.h"
+#include <entity.h>
+// #include <camera.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
